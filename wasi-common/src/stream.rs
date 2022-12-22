484077baa75153c0883869796171b99bf4926f30
Dan Gohman <dev@sunfishcode.online>
2022-12-22 15:30:30 -0800

    Add more documentation comments.

--- wasi-common/src/stream.rs
+++ wasi-common/src/stream.rs
@@ -1,34 +1,50 @@
 use crate::{Error, ErrorExt};
 use std::any::Any;
 
+/// A pseudo-stream.
+///
+/// This is "pseudo" because the real streams will be a type in wit, and
+/// built into the wit bindings, and will support async and type parameters.
+/// This pseudo-stream abstraction is synchronous and only supports bytes.
 #[async_trait::async_trait]
 pub trait WasiStream: Send + Sync {
     fn as_any(&self) -> &dyn Any;
 
+    /// If this stream is reading from a host file descriptor, return it so
+    /// that it can be polled with a host poll.
     #[cfg(unix)]
     fn pollable_read(&self) -> Option<rustix::fd::BorrowedFd> {
         None
     }
 
+    /// If this stream is reading from a host file descriptor, return it so
+    /// that it can be polled with a host poll.
     #[cfg(windows)]
     fn pollable_read(&self) -> Option<io_extras::os::windows::RawHandleOrSocket> {
         None
     }
 
+    /// If this stream is writing from a host file descriptor, return it so
+    /// that it can be polled with a host poll.
     #[cfg(unix)]
     fn pollable_write(&self) -> Option<rustix::fd::BorrowedFd> {
         None
     }
 
+    /// If this stream is writing from a host file descriptor, return it so
+    /// that it can be polled with a host poll.
     #[cfg(windows)]
     fn pollable_write(&self) -> Option<io_extras::os::windows::RawHandleOrSocket> {
         None
     }
 
+    /// Read bytes. On success, returns a pair holding the number of bytes read
+    /// and a flag indicating whether the end of the stream was reached.
     async fn read(&mut self, _buf: &mut [u8]) -> Result<(u64, bool), Error> {
         Err(Error::badf())
     }
 
+    /// Vectored-I/O form of `read`.
     async fn read_vectored<'a>(
         &mut self,
         _bufs: &mut [std::io::IoSliceMut<'a>],
@@ -36,22 +52,29 @@ pub trait WasiStream: Send + Sync {
         Err(Error::badf())
     }
 
+    /// Test whether vectored I/O reads are known to be optimized in the
+    /// underlying implementation.
     fn is_read_vectored(&self) -> bool {
         false
     }
 
+    /// Write bytes. On success, returns the number of bytes written.
     async fn write(&mut self, _buf: &[u8]) -> Result<u64, Error> {
         Err(Error::badf())
     }
 
+    /// Vectored-I/O form of `write`.
     async fn write_vectored<'a>(&mut self, _bufs: &[std::io::IoSlice<'a>]) -> Result<u64, Error> {
         Err(Error::badf())
     }
 
+    /// Test whether vectored I/O writes are known to be optimized in the
+    /// underlying implementation.
     fn is_write_vectored(&self) -> bool {
         false
     }
 
+    /// Transfer bytes directly from an input stream to an output stream.
     async fn splice(&mut self, dst: &mut dyn WasiStream, nelem: u64) -> Result<(u64, bool), Error> {
         let mut nspliced = 0;
         let mut saw_end = false;
@@ -71,6 +94,7 @@ pub trait WasiStream: Send + Sync {
         Ok((nspliced, saw_end))
     }
 
+    /// Read bytes from a stream and discard them.
     async fn skip(&mut self, nelem: u64) -> Result<(u64, bool), Error> {
         let mut nread = 0;
         let mut saw_end = false;
@@ -88,6 +112,7 @@ pub trait WasiStream: Send + Sync {
         Ok((nread, saw_end))
     }
 
+    /// Repeatedly write a byte to a stream.
     async fn write_repeated(&mut self, byte: u8, nelem: u64) -> Result<u64, Error> {
         let mut nwritten = 0;
 
@@ -103,12 +128,15 @@ pub trait WasiStream: Send + Sync {
         Ok(nwritten)
     }
 
+    /// Return the number of bytes that may be read without blocking.
     async fn num_ready_bytes(&self) -> Result<u64, Error> {
         Ok(0)
     }
 
+    /// Test whether this stream is readable.
     async fn readable(&self) -> Result<(), Error>;
 
+    /// Test whether this stream is writeable.
     async fn writable(&self) -> Result<(), Error>;
 }
 
