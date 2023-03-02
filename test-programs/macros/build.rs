c27729fe43728b99fdb7f14ca9c7540d0fd3f55a
Pat Hickey <phickey@fastly.com>
2023-03-02 14:37:32 -0800

    add a test for using wasi in a reactor

--- test-programs/macros/build.rs
+++ test-programs/macros/build.rs
@@ -1,3 +1,4 @@
+use heck::ToSnakeCase;
 use std::env;
 use std::fs;
 use std::path::PathBuf;
@@ -6,8 +7,6 @@ use wit_component::ComponentEncoder;
 
 fn main() {
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-
-    let mut components = Vec::new();
 
     println!("cargo:rerun-if-changed=../../src");
     let mut cmd = Command::new("cargo");
@@ -21,24 +20,29 @@ fn main() {
     let status = cmd.status().unwrap();
     assert!(status.success());
 
-    let wasi_adapter = out_dir.join("wasm32-unknown-unknown/release/wasi_snapshot_preview1.wasm");
-    println!("wasi adapter: {:?}", &wasi_adapter);
-    let wasi_adapter = fs::read(&wasi_adapter).unwrap();
+    let command_adapter =
+        out_dir.join("wasm32-unknown-unknown/release/wasi_snapshot_preview1.wasm");
+    println!("wasi command adapter: {:?}", &command_adapter);
+    let command_adapter = fs::read(&command_adapter).unwrap();
 
-    println!("cargo:rerun-if-changed=..");
-    let mut cmd = Command::new("rustup");
-    cmd.arg("run")
-        .arg("nightly")
-        .arg("cargo")
-        .arg("build")
-        .arg("--target=wasm32-wasi")
-        .current_dir("..")
+    println!("cargo:rerun-if-changed=../../src");
+    let mut cmd = Command::new("cargo");
+    cmd.arg("build")
+        .arg("--release")
+        .current_dir("../../")
+        .arg("--target=wasm32-unknown-unknown")
         .env("CARGO_TARGET_DIR", &out_dir)
-        .env("CARGO_PROFILE_DEV_DEBUG", "1")
         .env_remove("CARGO_ENCODED_RUSTFLAGS");
     let status = cmd.status().unwrap();
     assert!(status.success());
 
+    let reactor_adapter =
+        out_dir.join("wasm32-unknown-unknown/release/wasi_snapshot_preview1.wasm");
+    println!("wasi reactor adapter: {:?}", &reactor_adapter);
+    let reactor_adapter = fs::read(&reactor_adapter).unwrap();
+
+    // Build all test program crates:
+    println!("cargo:rerun-if-changed=..");
     let mut cmd = Command::new("rustup");
     cmd.arg("run")
         .arg("nightly")
@@ -47,6 +51,7 @@ fn main() {
         .arg("--target=wasm32-wasi")
         .arg("--package=wasi-tests")
         .arg("--package=test-programs")
+        .arg("--package=reactor-tests")
         .current_dir("..")
         .env("CARGO_TARGET_DIR", &out_dir)
         .env("CARGO_PROFILE_DEV_DEBUG", "1")
@@ -54,19 +59,25 @@ fn main() {
     let status = cmd.status().unwrap();
     assert!(status.success());
 
-    for file in out_dir.join("wasm32-wasi/debug").read_dir().unwrap() {
-        let file = file.unwrap().path();
-        if file.extension().and_then(|s| s.to_str()) != Some("wasm") {
-            continue;
-        }
-        let stem = file.file_stem().unwrap().to_str().unwrap().to_string();
+    let meta = cargo_metadata::MetadataCommand::new().exec().unwrap();
+
+    let mut command_components = Vec::new();
+
+    for stem in targets_in_package(&meta, "test-programs", "bin").chain(targets_in_package(
+        &meta,
+        "wasi-tests",
+        "bin",
+    )) {
+        let file = out_dir
+            .join("wasm32-wasi/debug")
+            .join(format!("{stem}.wasm"));
 
         let module = fs::read(&file).expect("read wasm module");
         let component = ComponentEncoder::default()
             .module(module.as_slice())
             .unwrap()
             .validate(true)
-            .adapter("wasi_snapshot_preview1", &wasi_adapter)
+            .adapter("wasi_snapshot_preview1", &command_adapter)
             .unwrap()
             .encode()
             .expect(&format!(
@@ -75,9 +86,53 @@ fn main() {
             ));
         let component_path = out_dir.join(format!("{}.component.wasm", &stem));
         fs::write(&component_path, component).expect("write component to disk");
-        components.push((stem, component_path));
+        command_components.push((stem, component_path));
     }
 
-    let src = format!("const COMPONENTS: &[(&str, &str)] = &{:?};", components);
+    let mut reactor_components = Vec::new();
+
+    for stem in targets_in_package(&meta, "reactor-tests", "cdylib") {
+        let stem = stem.to_snake_case();
+        let file = out_dir
+            .join("wasm32-wasi/debug")
+            .join(format!("{stem}.wasm"));
+
+        let module = fs::read(&file).expect(&format!("read wasm module: {file:?}"));
+        let component = ComponentEncoder::default()
+            .module(module.as_slice())
+            .unwrap()
+            .validate(true)
+            .adapter("wasi_snapshot_preview1", &reactor_adapter)
+            .unwrap()
+            .encode()
+            .expect(&format!(
+                "module {:?} can be translated to a component",
+                file
+            ));
+        let component_path = out_dir.join(format!("{}.component.wasm", &stem));
+        fs::write(&component_path, component).expect("write component to disk");
+        reactor_components.push((stem, component_path));
+    }
+
+    let src = format!(
+        "const COMMAND_COMPONENTS: &[(&str, &str)] = &{command_components:?};
+         const REACTOR_COMPONENTS: &[(&str, &str)] = &{reactor_components:?};
+        ",
+    );
     std::fs::write(out_dir.join("components.rs"), src).unwrap();
+}
+
+fn targets_in_package<'a>(
+    meta: &'a cargo_metadata::Metadata,
+    package: &'a str,
+    kind: &'a str,
+) -> impl Iterator<Item = &'a String> + 'a {
+    meta.packages
+        .iter()
+        .find(|p| p.name == package)
+        .unwrap()
+        .targets
+        .iter()
+        .filter(move |t| t.kind == &[kind])
+        .map(|t| &t.name)
 }
