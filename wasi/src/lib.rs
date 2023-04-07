97a6314aff170bca30554f055987156d25546b42
Roman Volosatovs <rvolosatovs@riseup.net>
2023-04-07 21:23:06 +0200

    fix: symlink `wit`, rather than using relative paths
    This enables downstream builds utilizing `cargo vendor`, since
    `cargo vendor` resolves symlinks before packaging crates up
    
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- wasi/src/lib.rs
+++ wasi/src/lib.rs
@@ -4,7 +4,6 @@ pub mod http;
 pub mod snapshots {
     pub mod preview_2 {
         wit_bindgen::generate!({
-            path: "../wit",
             world: "reactor",
             std_feature,
         });
