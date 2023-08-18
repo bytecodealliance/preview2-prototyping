0852aa51f31b17c315f358014ef8347a7d12010d
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2023-08-18 02:24:20 +0200

    feat: update wasi http client to latest

--- wasi/src/http_client.rs
+++ wasi/src/http_client.rs
@@ -3,40 +3,43 @@ use bytes::{BufMut, Bytes, BytesMut};
 use core::ops::Deref;
 use http::header::{HeaderName, HeaderValue};
 
-use crate::snapshots::preview_2::{default_outgoing_http, streams, types as http_types};
+use crate::snapshots::preview_2::wasi::http::{outgoing_handler, types as http_types};
+use crate::snapshots::preview_2::wasi::io::streams;
+use crate::snapshots::preview_2::wasi::poll::poll;
 
 pub struct DefaultClient {
-    options: Option<default_outgoing_http::RequestOptions>,
+    options: Option<outgoing_handler::RequestOptions>,
 }
 
 impl DefaultClient {
-    pub fn new(options: Option<default_outgoing_http::RequestOptions>) -> Self {
+    pub fn new(options: Option<outgoing_handler::RequestOptions>) -> Self {
         Self { options }
     }
 
     pub fn handle(&self, req: http::Request<Bytes>) -> anyhow::Result<http::Response<Bytes>> {
-        let req = Request::try_from(req)
-            .context("converting http request")?
-            .to_owned();
+        let req = Request::try_from(req).context("converting http request")?;
 
-        let res = default_outgoing_http::handle(req, self.options);
-        http_types::drop_outgoing_request(req);
+        let res = outgoing_handler::handle(req.id, self.options);
 
         let response =
             http::Response::try_from(Response(res)).context("converting http response")?;
-        http_types::drop_incoming_response(res);
+
+        streams::drop_output_stream(req.body);
+        http_types::drop_outgoing_request(req.id);
 
         Ok(response)
     }
 }
 
-pub struct Request(default_outgoing_http::OutgoingRequest);
+#[derive(Default, Debug, Clone)]
+pub struct Request {
+    id: outgoing_handler::OutgoingRequest,
+    body: http_types::OutgoingStream,
+}
 
-impl Deref for Request {
-    type Target = default_outgoing_http::OutgoingRequest;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
+impl Request {
+    pub fn new(id: outgoing_handler::OutgoingRequest, body: http_types::OutgoingStream) -> Self {
+        Self { id, body }
     }
 }
 
@@ -45,70 +48,66 @@ impl TryFrom<http::Request<Bytes>> for Request {
 
     fn try_from(value: http::Request<Bytes>) -> Result<Self, Self::Error> {
         let (parts, body) = value.into_parts();
-        let path = parts.uri.path();
-        let query = parts.uri.query();
-        let method = Method::try_from(parts.method)?;
+        let method = Method::try_from(parts.method).context("converting request method")?;
+        let path_with_query = parts.uri.path_and_query();
         let headers = Headers::from(&parts.headers);
         let scheme = match parts.uri.scheme_str().unwrap_or("") {
-            "http" => Some(http_types::SchemeParam::Http),
-            "https" => Some(http_types::SchemeParam::Https),
+            "http" => Some(&http_types::Scheme::Http),
+            "https" => Some(&http_types::Scheme::Https),
             _ => None,
         };
         let request = http_types::new_outgoing_request(
-            method.to_owned(),
-            path,
-            query.unwrap_or(""),
+            &method,
+            path_with_query.map(|q| q.as_str()),
             scheme,
-            parts
-                .uri
-                .authority()
-                .map(|a| a.as_str())
-                .ok_or_else(|| anyhow!("unable to extract authority"))?,
+            parts.uri.authority().map(|a| a.as_str()),
             headers.to_owned(),
         );
 
         let request_body = http_types::outgoing_request_write(request)
             .map_err(|_| anyhow!("outgoing request write failed"))?;
 
-        let mut body_cursor = 0;
         if body.is_empty() {
             streams::write(request_body, &[]).map_err(|_| anyhow!("writing request body"))?;
         } else {
+            let output_stream_pollable = streams::subscribe_to_output_stream(request_body);
+            let mut body_cursor = 0;
             while body_cursor < body.len() {
-                let written = streams::write(request_body, &body[body_cursor..])
+                let (written, _) = streams::write(request_body, &body[body_cursor..])
                     .map_err(|_| anyhow!("writing request body"))?;
                 body_cursor += written as usize;
             }
+            poll::drop_pollable(output_stream_pollable);
         }
 
-        Ok(Request(request))
+        Ok(Request::new(request, request_body))
     }
 }
 
-pub struct Method<'a>(http_types::MethodParam<'a>);
+pub struct Method(http_types::Method);
 
-impl<'a> Deref for Method<'a> {
-    type Target = http_types::MethodParam<'a>;
+impl Deref for Method {
+    type Target = http_types::Method;
 
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-impl<'a> TryFrom<http::Method> for Method<'a> {
+impl TryFrom<http::Method> for Method {
     type Error = anyhow::Error;
 
     fn try_from(method: http::Method) -> Result<Self, Self::Error> {
         Ok(Self(match method {
-            http::Method::GET => http_types::MethodParam::Get,
-            http::Method::POST => http_types::MethodParam::Post,
-            http::Method::PUT => http_types::MethodParam::Put,
-            http::Method::DELETE => http_types::MethodParam::Delete,
-            http::Method::PATCH => http_types::MethodParam::Patch,
-            http::Method::CONNECT => http_types::MethodParam::Connect,
-            http::Method::TRACE => http_types::MethodParam::Trace,
-            http::Method::HEAD => http_types::MethodParam::Head,
-            http::Method::OPTIONS => http_types::MethodParam::Options,
+            http::Method::GET => http_types::Method::Get,
+            http::Method::POST => http_types::Method::Post,
+            http::Method::PUT => http_types::Method::Put,
+            http::Method::DELETE => http_types::Method::Delete,
+            http::Method::PATCH => http_types::Method::Patch,
+            http::Method::CONNECT => http_types::Method::Connect,
+            http::Method::TRACE => http_types::Method::Trace,
+            http::Method::HEAD => http_types::Method::Head,
+            http::Method::OPTIONS => http_types::Method::Options,
             _ => return Err(anyhow!("failed due to unsupported method, currently supported methods are: GET, POST, PUT, DELETE, PATCH, CONNECT, TRACE, HEAD, and OPTIONS")),
         }))
     }
@@ -129,45 +128,62 @@ impl TryFrom<Response> for http::Response<Bytes> {
 
     fn try_from(value: Response) -> Result<Self, Self::Error> {
         let future_response = value.to_owned();
-        // TODO: we could create a pollable from the future_response and
-        // poll on it here to test that its available immediately
-        // poll::drop_pollable(future_response);
 
-        let incoming_response = http_types::future_incoming_response_get(future_response)
-            .ok_or_else(|| anyhow!("incoming response is available immediately"))?
-            .map_err(|e| anyhow!("incoming response error: {e:?}"))?;
+        let incoming_response = match http_types::future_incoming_response_get(future_response) {
+            Some(result) => result,
+            None => {
+                let pollable = http_types::listen_to_future_incoming_response(future_response);
+                let _ = poll::poll_oneoff(&[pollable]);
+                http_types::future_incoming_response_get(future_response)
+                    .expect("incoming response available")
+            }
+        }
+        .map_err(|e| anyhow!("incoming response error: {e:?}"))?;
+
         http_types::drop_future_incoming_response(future_response);
 
         let status = http_types::incoming_response_status(incoming_response);
-        let headers_handle = http_types::incoming_response_headers(incoming_response);
 
         let body_stream = http_types::incoming_response_consume(incoming_response)
             .map_err(|_| anyhow!("consuming incoming response"))?;
+        let input_stream_pollable = streams::subscribe_to_input_stream(body_stream);
 
         let mut body = BytesMut::new();
-        let mut eof = false;
-        while !eof {
-            let (body_chunk, stream_ended) = streams::read(body_stream, u64::MAX)
-                .map_err(|_| anyhow!("reading response body"))?;
-            eof = stream_ended;
+        let mut eof = streams::StreamStatus::Open;
+        while eof != streams::StreamStatus::Ended {
+            let (body_chunk, stream_status) = streams::read(body_stream, u64::MAX)
+                .map_err(|e| anyhow!("reading response body: {e:?}"))?;
+            eof = if body_chunk.is_empty() {
+                streams::StreamStatus::Ended
+            } else {
+                stream_status
+            };
             body.put(body_chunk.as_slice());
         }
+        poll::drop_pollable(input_stream_pollable);
+
         let mut res = http::Response::builder()
             .status(status)
             .body(body.freeze())
             .map_err(|_| anyhow!("building http response"))?;
 
+        streams::drop_input_stream(body_stream);
+
+        let headers_handle = http_types::incoming_response_headers(incoming_response);
         if headers_handle > 0 {
             let headers_map = res.headers_mut();
             for (name, value) in http_types::fields_entries(headers_handle) {
                 headers_map.insert(
                     HeaderName::from_bytes(name.as_bytes())
                         .map_err(|_| anyhow!("converting response header name"))?,
-                    HeaderValue::from_str(value.as_str())
+                    HeaderValue::from_bytes(value.as_slice())
                         .map_err(|_| anyhow!("converting response header value"))?,
                 );
             }
         }
+        http_types::drop_fields(headers_handle);
+
+        http_types::drop_incoming_response(incoming_response);
 
         Ok(res)
     }
@@ -186,10 +202,11 @@ impl Deref for Headers {
 impl<'a> From<&'a http::HeaderMap> for Headers {
     fn from(headers: &'a http::HeaderMap) -> Self {
         Self(http_types::new_fields(
-            &headers
+            headers
                 .iter()
-                .map(|(name, value)| (name.as_str(), value.to_str().unwrap()))
-                .collect::<Vec<(&'a str, &'a str)>>(),
+                .map(|(name, value)| (name.to_string(), value.to_str().unwrap().to_string()))
+                .collect::<Vec<_>>()
+                .as_slice(),
         ))
     }
 }
--- wasi/src/lib.rs
+++ wasi/src/lib.rs
@@ -4,8 +4,9 @@ pub mod http_client;
 pub mod snapshots {
     pub mod preview_2 {
         wit_bindgen::generate!({
-            world: "reactor",
+            world: "wasi:preview/reactor",
             std_feature,
+            ownership: Owning,
         });
     }
 }
